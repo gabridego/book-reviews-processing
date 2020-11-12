fd4257944e9dabe42b851e2c7052253eaa284ba1
privitec <concetto-antonino.privitera@grenoble-inp.org>
2020-11-12 17:11:10 +0100

    code: insert query

--- node_apiserver/results_dao.js
+++ node_apiserver/results_dao.js
@@ -22,3 +22,30 @@ exports.getWordCount = function() {
         });
     });
 }
+
+exports.insertTrainingDocuments = function(document) {
+    return new Promise((resolve, reject) => {
+        /*const sql = "SELECT id, name FROM categories;";
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject({msg: "error in db", details: err});
+            } else {
+                let categories = rows.map((row) => createCategory(row));
+                resolve(categories);
+            }
+        });*/
+
+        MongoClient.connect(url, function(err, db) {
+            if (err) 
+                reject({msg: "error in db", details: err});
+            var dbo = db.db(dbname);
+            var myobj = { name: "Company Inc", address: "Highway 37" };
+            dbo.collection("customers").insertOne(document, function(err, res) {
+                if (err) 
+                    reject({msg: "error in db", details: err});
+                resolve(res);
+                db.close();
+            });
+        });
+    });
+}
--- node_apiserver/server.js
+++ node_apiserver/server.js
@@ -36,6 +36,15 @@ app.get('/api/results/sentiment/accuracy', (req, res) => {
 });
 
 app.post('/api/training/documents', (req, res) => {
+    const data = req.body;
+
+    if(!data){
+        res.status(400).end();
+    } else {
+        resultsDao.insertTrainingDocuments(data)
+            .then((id) => res.status(201).json({"id" : id}))
+            .catch((err) => { res.status(500).json(err) });
+    }
 });
 
 app.post('/api/test/documents', (req, res) => {
