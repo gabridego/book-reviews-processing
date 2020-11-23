8f6fad9e8f6b0ed9a6efead95183180c861d26cc
chaloupm <maxime.chaloupe@grenoble-inp.org>
2020-11-23 12:45:37 +0100

    terraform + kops

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "kube_bucket" {
+  bucket = "sdtd-kubernetes-state"
+  acl = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # otherwise you cannot destroy s3 instance with terraform destroy
+  force_destroy = true
+
+  tags = {
+    Name = "Kubernetes State"
+    Environment = "Dev"
+  }
+}
