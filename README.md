3d04dd1f9f122e3763cb2f36d293e1d15dcff1df
Concetto Antonino Privitera <s270436@studenti.polito.it>
2020-11-28 00:00:01 +0100

    code: fixes

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 
 3) kubectl create configmap webserver-env-file --from-env-file=./webserver-env-file.properties --save-config
 4) kubectl get configmap webserver-env-file -o yaml > ./webserver-configmap.yaml
+5) name of db service should be mongo
--- components/mongo-seed/import.sh
+++ components/mongo-seed/import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mongoimport --host mongo --db sdtddb --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
-mongoimport --host mongo --db sdtddb --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
-mongoimport --host mongo --db sdtddb --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
+mongoimport --host db --db sdtddb --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
+mongoimport --host db --db sdtddb --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
+mongoimport --host db --db sdtddb --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
