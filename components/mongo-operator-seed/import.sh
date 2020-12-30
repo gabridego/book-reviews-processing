c625c66e1231ac8a90ff2225ec65cc129466f9fc
chaloupm <maxime.chaloupe@grenoble-inp.org>
2020-12-30 18:29:44 +0100

    Import to replicaset easily
    Some warnings in logs but no errors unless duplicate key

--- components/mongo-operator-seed/import.sh
+++ components/mongo-operator-seed/import.sh
@@ -8,6 +8,15 @@
 #mongoimport --uri mongodb://my-user:userPassword@$DB_SERVICE:27017/admin --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
 #mongoimport --uri mongodb://my-user:userPassword@$DB_SERVICE:27017/admin --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
 
-mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
-mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
-mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
+#mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
+#mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
+#mongoimport --host $DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
+
+#mongoimport --uri mongodb://my-user:userPassword@$DB_SERVICE.default.svc.cluster.local:27017/admin?replicaSet=db --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
+#mongoimport --uri mongodb://my-user:userPassword@$DB_SERVICE.default.svc.cluster.local:27017/admin?replicaSet=db --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
+#mongoimport --uri mongodb://my-user:userPassword@$DB_SERVICE.default.svc.cluster.local:27017/admin?replicaSet=db --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
+
+mongoimport --host db/$DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection wordcount --type json --file /mongo-seed/wc.json --jsonArray
+mongoimport --host db/$DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection sentiment --type json --file /mongo-seed/reviews.json --jsonArray
+mongoimport --host db/$DB_SERVICE --port 27017 --username my-user --password userPassword --authenticationDatabase admin --db sdtddb --collection accuracy --type json --file /mongo-seed/accuracies.json --jsonArray
+
